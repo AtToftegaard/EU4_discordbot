b71d43b277f0ac9505c1a70260c4f0b26be66db1
Shemiroth <42591036+Shemiroth@users.noreply.github.com>
2018-11-08 23:33:53 +0000

    Added roles to bot
    Add roles to bot and checks if the country exists.

---
+++ checkFile.py
@@ -0,0 +1,21 @@
+import os, sys, io, tkinter, re, pandas
+
+def check_country(file, country):
+    openFile = open(file)
+    pattern = re.compile('.*(%s).*'%country)
+   
+    for line in openFile:
+        if(re.match(pattern, line)):
+            return True
+
+
+def main():
+    print("importing file: " +sys.argv[1])
+    file = open(sys.argv[1])
+    country = "Assam"
+    if(check_country(file, country)):
+        print("true")
+
+if __name__ == '__main__':
+    main()
+
